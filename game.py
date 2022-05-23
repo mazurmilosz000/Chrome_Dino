e84915edce801fd17f16c7f6c8813d426331c108
Milosz Mazur <mazur.milosz000@gmail.com>
2022-05-23 23:35:05 +0200

    first commit

---
+++ game.py
@@ -0,0 +1,29 @@
+import pygame
+from settings import *
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode(WINDOW_SIZE)
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption("Chrome Dino!")
+        self.running = True
+
+    def run(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        self.running = False
+
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+
+if __name__ == '__main__':
+
+    game = Game()
+    game.run()
